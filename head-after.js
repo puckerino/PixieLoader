c9eee0ae7a7d9eea2a24dc2a176ad209769bccf7
puckerino <loregalafate+github@gmail.com>
2026-07-02 09:34:48 +0200

    Update head-after.js

--- head-after.js
+++ head-after.js
@@ -3,6 +3,7 @@
 (function () {
   function start() {
     window.PixieLoader.loadStyles(window.PIXIE.styles.afterForumCSS);
+    window.PixieLoader.loadStyles(window.PIXIE.themeStyles?.afterForumCSS, window.PIXIE.themeBase);
   }
 
   if (window.PixieLoader) start();
