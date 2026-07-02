8aa62309815158036687de766a39a31e8edefc9d
puckerino <loregalafate+github@gmail.com>
2026-07-02 09:32:42 +0200

    Update head-before.js

--- head-before.js
+++ head-before.js
@@ -1,6 +1,7 @@
 (function () {
   function start() {
     window.PixieLoader.loadStyles(window.PIXIE.styles.beforeForumCSS);
+    window.PixieLoader.loadStyles(window.PIXIE.themeStyles?.beforeForumCSS, window.PIXIE.themeBase);
   }
 
   if (window.PixieLoader) start();
